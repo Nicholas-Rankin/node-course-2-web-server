6703a9295ab1ea9e8f06ecfa9dd8d7e54127b6ca
Rankin <Nicholas.Rankin@inin.com>
2016-11-30 13:13:47 -0500

    Added Projects page

--- server.js
+++ server.js
@@ -45,13 +45,20 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
+    pageTitle: 'About Page'
   });
 });
 
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to fulfill this request.'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Project Page',
+    welcomeMessage: 'Insert Project portfolio here.'
   });
 });
 
